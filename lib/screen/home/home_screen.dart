9ea2707de703ca6d826b1df60b0a0e5b70d94aeb
ali-susanto <alisusanto.main@gmail.com>
2022-09-08 19:25:04 +0700

    add bottom navbar

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -12,38 +12,77 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-          child: Column(
-            children: [
-              Container(
-                width: size.width,
-                color: Colors.blueAccent,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text('129 Calories'),
-                        Text('129 Calories'),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text('129 Calories')
-                  ],
-                ),
-              )
-            ],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            child: Column(
+              children: [
+                Container(
+                  width: size.width,
+                  color: Colors.blueAccent,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('129 Calories'),
+                          Text('129 Calories'),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text('129 Calories')
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          elevation: 0,
+          child: const Icon(Icons.camera),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 6,
+          color: Colors.blue,
+          child: Container(
+            height: 50,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+            decoration: BoxDecoration(
+              //color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    offset: const Offset(0, -4),
+                    blurRadius: 20,
+                    color: Colors.grey.withOpacity(0.5))
+              ],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: SafeArea(
+                top: false,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.home),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.home),
+                    ),
+                  ],
+                )),
+          ),
+        ));
   }
 }
