f84641d36749274cc20bd1080699cd5a2c371b36
ali-susanto <alisusanto.main@gmail.com>
2022-09-21 19:48:02 +0700

    add constant color

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryBlue = Color(0xFF4163b1);
+const kSecondaryBlue = Color(0xFF83a9e4);
+const kWhiteColor = Color(0xFFf5f3fb);
+const kBlackColor = Color(0xFF020507);
+const kGreyColor = Color(0xFFa9b3b9);
+const kPrimaryGreen = Color(0xFF24444b);
+const kSecondaryGreen = Color(0xFF769152);
