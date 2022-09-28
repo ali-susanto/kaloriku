972ee26d39a38db4efbe03d27483f0f19bc5ad3b
ali-susanto <alisusanto.main@gmail.com>
2022-09-28 17:34:03 +0700

    add tips view model with provider

---
+++ lib/screen/tips/tips_view_model.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:kaloriku/enums.dart';
+import 'package:kaloriku/model/tips_model.dart';
+import 'package:kaloriku/service/tips_api.dart';
+
+class TipsViewModel with ChangeNotifier {
+  List<TipsModel> _tipsData = [];
+  List<TipsModel> get tipsData => _tipsData;
+
+  DataState _stateType = DataState.loading;
+  DataState get stateType => _stateType;
+
+  String errorMessage = '';
+
+  void changeState(DataState s) {
+    _stateType = s;
+    notifyListeners();
+  }
+
+  void getData() async {
+    try {
+      changeState(DataState.loading);
+      List<TipsModel>? listData = await TipsApi().getTipsData();
+
+      _tipsData = listData;
+      notifyListeners();
+      changeState(DataState.succes);
+    } catch (e) {
+      changeState(DataState.error);
+      debugPrint(e.toString());
+      errorMessage = e.toString();
+      notifyListeners();
+    }
+  }
+}
