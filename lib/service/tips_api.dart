5e4395bc7a73f9179c42e8a1f6ce97f1fca89765
ali-susanto <alisusanto.main@gmail.com>
2022-09-28 17:33:16 +0700

    add mapping and return to list in function get data from api

--- lib/service/tips_api.dart
+++ lib/service/tips_api.dart
@@ -1,11 +1,21 @@
 import 'package:dio/dio.dart';
+import 'package:kaloriku/model/tips_model.dart';
 
 class TipsApi {
   final Dio dio = Dio();
   final baseUrl = 'https://62b809d5f4cb8d63df57c091.mockapi.io/healthy';
 
-  Future<Response> getTipsData() async {
-    Response response = await dio.get(baseUrl);
-    return response;
+  Future<List<TipsModel>> getTipsData() async {
+    try {
+      Response<List<dynamic>> response = await dio.get(baseUrl);
+      if (response.statusCode == 200) {
+        List<TipsModel>? tipsData =
+            response.data?.map((e) => TipsModel.fromJson(e)).toList();
+        return tipsData ?? [];
+      }
+    } on DioError catch (e) {
+      e.message.toString();
+    }
+    return [];
   }
 }
